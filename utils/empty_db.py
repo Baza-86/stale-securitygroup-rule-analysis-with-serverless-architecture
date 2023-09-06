1f7b30de889ded10336f301f98744d7e382fa7ef
Kameron S <kameron.shinmar@cgi.com>
2023-09-06 15:45:26 +0100

    #63 update script

--- utils/empty_db.py
+++ utils/empty_db.py
@@ -7,17 +7,27 @@ def deleteDBB():
         session = boto3.session.Session()
         client = session.client('dynamodb')
         ddb_table = os.environ.get('DB_TABLE')
+        delete_items(client=client, ddb_table=ddb_table)
+           
+    else:
+        print("Please set the DB_TABLE Enviroment Varible!")
+
+def delete_items(client, ddb_table):
+    lastevalutedkey = "start"
+    while lastevalutedkey != None:
         items = list_dbb_items(client, ddb_table=ddb_table, IndexName="protocol")
+        for item in items['Items']:
+            delete_ddb_item(client=client, item=item, ddb_table=ddb_table)
         if items['LastEvaluatedKey']:
             lastevalutedkey = items['LastEvaluatedKey']
-            for item in items['Items']:
-                delete_ddb_item(client=client, item=item, ddb_table=ddb_table)
+        else:
+            print("Setting lastevalutedkey is too none..")
+            lastevalutedkey = None
+            print("lastevalutedkey is now none..")
+
 
         print(items['Count'])
         print(items['LastEvaluatedKey'])
-                
-    else:
-        print("Please set the DB_TABLE Enviroment Varible!")
     
 if __name__ == "__main__":
     deleteDBB()
