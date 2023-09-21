3d071a066dfb72e928ffaf2f5215ea2677fc72b9
Kameron S <kameron.shinmar@cgi.com>
2023-09-21 17:09:15 +0100

    #74 update lambda and cf

--- lambda_functions/PayloadCreator/lambda_function.py
+++ lambda_functions/PayloadCreator/lambda_function.py
@@ -1,19 +1,30 @@
 import json
+from datetime import datetime, timedelta, date
 
 def lambda_handler(event, context):
-    outputFileArray = event['outputFileArray']
-    outputCsv = event['outputCsv']
-    outputFileArray.append(outputCsv)
-    outputRows = event['outputRows']
-    
-    queryParams = event['queryParams']
-    
-    queryParams['queryOffset'] += 10
-    
-    payload_dict = {
+    print(event.keys())
+    if 'generatedDate' in event.keys(): 
+        outputFileArray = event['outputFileArray']
+        outputCsv = event['outputCsv']
+        outputFileArray.append(outputCsv)
+        outputRows = event['outputRows']
+        
+        queryParams = event['queryParams']
+        
+        queryParams['queryOffset'] += 10
+        
+        payload_dict = {
         'queryParams': queryParams,
         'outputFileArray': outputFileArray,
         'outputRows': outputRows
     }
+    else:
+        print("date to be added to output")
+        date_yst = str(date.today() - timedelta(1))
+        payload_dict = {
+        'date': date_yst[-2:]
+    }
+    
+    
     
     return payload_dict
