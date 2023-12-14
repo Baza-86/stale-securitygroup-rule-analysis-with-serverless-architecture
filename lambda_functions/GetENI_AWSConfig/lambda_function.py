a78a99a9fcb5408c187f7e01efb2223fce60b398
Kameron S <kameron.shinmar@cgi.com>
2023-12-14 15:58:24 +0000

    #114 submod/symlink aws_discovery

---
+++ lambda_functions/GetENI_AWSConfig/lambda_function.py
@@ -0,0 +1,54 @@
+import json
+import os
+from modules.src.aws_discovery import awsdiscover
+##from modules.aws_network.export import ExportNetwork
+from datetime import datetime
+import time
+import calendar
+
+exp = ExportNetwork()
+
+def lambda_handler(event, context):
+    if os.environ.get('DB_TABLE'):
+
+        accountNo = event.get('AccountNo')
+        CrossAccountRoleName = os.environ.get('CROSS_ACCOUNT_ROLE_NAME')
+        if accountNo:
+            ArnRole = f"arn:aws:iam::{accountNo}:role/{CrossAccountRoleName}"
+            print("Using role: " + ArnRole)
+            nic = NetworkInterface(role_arn=ArnRole, role_session_name="AssumedRoleSessionName")
+        else:
+            nic = NetworkInterface()
+        
+        nic.list_interfaces()
+        
+        interface_details_list = []
+    
+        for n in nic.iface_ids:
+            interface_details_list.append(nic.get_interface(nic_id=[n]))
+        
+        for i in interface_details_list:
+            try:
+                i.attachment_properties['AttachTime']=calendar.timegm(i.attachment_properties['AttachTime'].utctimetuple())
+            except:
+                pass
+    
+        # print(interface_details_list)
+    
+        try:
+            response = exp.write_ddb(ddb_table=os.environ['DB_TABLE'],input_list=interface_details_list)
+            return response
+        except Exception as e:
+            error_msg = {
+                "message": e.response
+            }
+            print(error_msg)
+            return {
+                "message": e.response
+            }
+    else:
+        print("Please set the DB_TABLE Enviroment Variable!")
+        return {
+            "message": "Please set the DB_TABLE Enviroment Variable!"
+        }
+    
