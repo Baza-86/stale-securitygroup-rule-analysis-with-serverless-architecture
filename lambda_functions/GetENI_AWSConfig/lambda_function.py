a7e534298fad9bb30ccbb17fa4fcc825fc4e4000
Kameron S <kameron.shinmar@cgi.com>
2024-01-05 14:54:57 +0000

    #114 testing aws config

--- lambda_functions/GetENI_AWSConfig/lambda_function.py
+++ lambda_functions/GetENI_AWSConfig/lambda_function.py
@@ -6,49 +6,60 @@ from datetime import datetime
 import time
 import calendar
 
-exp = ExportNetwork()
+region = "eu-west-2"
 
 def lambda_handler(event, context):
     if os.environ.get('DB_TABLE'):
 
-        accountNo = event.get('AccountNo')
+        accountNo = "XXXXXXXXXXX"
         CrossAccountRoleName = os.environ.get('CROSS_ACCOUNT_ROLE_NAME')
         if accountNo:
-            ArnRole = f"arn:aws:iam::{accountNo}:role/{CrossAccountRoleName}"
-            print("Using role: " + ArnRole)
-            nic = NetworkInterface(role_arn=ArnRole, role_session_name="AssumedRoleSessionName")
+            # ArnRole = f"arn:aws:iam::{accountNo}:role/{CrossAccountRoleName}"
+            # print("Using role: " + ArnRole)
+            awsconfig = awsdiscover.AwsDiscovery()
+            mylist = awsconfig.list_resources(resource_type="AWS::EC2::NetworkInterface")
+            resources = []
+            for r in mylist:
+                resources.append(r.resource_id)
+
+            print(resources)
+            myresource = awsconfig.batch_get_resource_config(resource_type="AWS::EC2::NetworkInterface", resource_id=resources)
+            print(mylist)
+            print(myresource)
         else:
-            nic = NetworkInterface()
+            pass
         
-        nic.list_interfaces()
+        # nic.list_interfaces()
         
-        interface_details_list = []
+        # interface_details_list = []
     
-        for n in nic.iface_ids:
-            interface_details_list.append(nic.get_interface(nic_id=[n]))
+        # for n in nic.iface_ids:
+        #     interface_details_list.append(nic.get_interface(nic_id=[n]))
         
-        for i in interface_details_list:
-            try:
-                i.attachment_properties['AttachTime']=calendar.timegm(i.attachment_properties['AttachTime'].utctimetuple())
-            except:
-                pass
+        # for i in interface_details_list:
+        #     try:
+        #         i.attachment_properties['AttachTime']=calendar.timegm(i.attachment_properties['AttachTime'].utctimetuple())
+        #     except:
+        #         pass
     
         # print(interface_details_list)
     
-        try:
-            response = exp.write_ddb(ddb_table=os.environ['DB_TABLE'],input_list=interface_details_list)
-            return response
-        except Exception as e:
-            error_msg = {
-                "message": e.response
-            }
-            print(error_msg)
-            return {
-                "message": e.response
-            }
+        # try:
+        #     response = exp.write_ddb(ddb_table=os.environ['DB_TABLE'],input_list=interface_details_list)
+        #     return response
+        # except Exception as e:
+        #     error_msg = {
+        #         "message": e.response
+        #     }
+        #     print(error_msg)
+        #     return {
+        #         "message": e.response
+        #     }
     else:
         print("Please set the DB_TABLE Enviroment Variable!")
         return {
             "message": "Please set the DB_TABLE Enviroment Variable!"
         }
-    
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
